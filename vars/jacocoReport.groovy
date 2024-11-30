09daad62b9cef5436bddd45b28338b16ab8f2c3c
naresh240 <naresh240.qis@gmail.com>
2024-11-30 22:18:48 +0530

    added jacoco report groovy

--- vars/jacocoReport.groovy
+++ vars/jacocoReport.groovy
@@ -1,69 +1,44 @@
+// vars/jacocoReport.groovy
 def call(Map config = [:]) {
     // Default configurations
-    def execPattern = config.get('execPattern', '**/jacoco.exec') // String
-    def classPattern = config.get('classPattern', '**/classes') // String
-    def sourcePattern = config.get('sourcePattern', '**/src/main/java') // String
+    def execPattern = config.get('execPattern', '**/jacoco.exec')
+    def classPattern = config.get('classPattern', '**/classes')
+    def sourcePattern = config.get('sourcePattern', '**/src/main/java')
 
-    def exclusionPattern = config.get('exclusionPattern')  // Optional, String
-    def inclusionPattern = config.get('inclusionPattern')  // Optional, String
-    def runAlways = config.get('runAlways', true)  // Optional, Boolean (default true)
-    def skipCopyOfSrcFiles = config.get('skipCopyOfSrcFiles', false)  // Optional, Boolean (default false)
-    def sourceExclusionPattern = config.get('sourceExclusionPattern')  // Optional, String
-    def buildOverBuild = config.get('buildOverBuild', false)  // Optional, Boolean (default false)
-    def changeBuildStatus = config.get('changeBuildStatus', false)  // Optional, Boolean (default false)
+    def exclusionPattern = config.get('exclusionPattern')  // Optional
+    def inclusionPattern = config.get('inclusionPattern')  // Optional
+    def runAlways = config.get('runAlways')  // Optional
+    def skipCopyOfSrcFiles = config.get('skipCopyOfSrcFiles')  // Optional
+    def sourceExclusionPattern = config.get('sourceExclusionPattern')  // Optional
+    def buildOverBuild = config.get('buildOverBuild')  // Optional
+    def changeBuildStatus = config.get('changeBuildStatus')  // Optional
 
     // Delta Thresholds (optional, no default values)
-    def deltaBranchCoverage = config.get('deltaBranchCoverage')  // Optional, Integer or String
-    def deltaClassCoverage = config.get('deltaClassCoverage')  // Optional, Integer or String
-    def deltaComplexityCoverage = config.get('deltaComplexityCoverage')  // Optional, Integer or String
-    def deltaInstructionCoverage = config.get('deltaInstructionCoverage')  // Optional, Integer or String
-    def deltaLineCoverage = config.get('deltaLineCoverage')  // Optional, Integer or String
-    def deltaMethodCoverage = config.get('deltaMethodCoverage')  // Optional, Integer or String
+    def deltaBranchCoverage = config.get('deltaBranchCoverage')  // Optional
+    def deltaClassCoverage = config.get('deltaClassCoverage')  // Optional
+    def deltaComplexityCoverage = config.get('deltaComplexityCoverage')  // Optional
+    def deltaInstructionCoverage = config.get('deltaInstructionCoverage')  // Optional
+    def deltaLineCoverage = config.get('deltaLineCoverage')  // Optional
+    def deltaMethodCoverage = config.get('deltaMethodCoverage')  // Optional
 
     // Minimum Coverage thresholds (optional, no default values)
-    def minimumBranchCoverage = config.get('minimumBranchCoverage')  // Optional, Integer or String
-    def minimumClassCoverage = config.get('minimumClassCoverage')  // Optional, Integer or String
-    def minimumComplexityCoverage = config.get('minimumComplexityCoverage')  // Optional, Integer or String
-    def minimumInstructionCoverage = config.get('minimumInstructionCoverage')  // Optional, Integer or String
-    def minimumLineCoverage = config.get('minimumLineCoverage')  // Optional, Integer or String
-    def minimumMethodCoverage = config.get('minimumMethodCoverage')  // Optional, Integer or String
+    def minimumBranchCoverage = config.get('minimumBranchCoverage')  // Optional
+    def minimumClassCoverage = config.get('minimumClassCoverage')  // Optional
+    def minimumComplexityCoverage = config.get('minimumComplexityCoverage')  // Optional
+    def minimumInstructionCoverage = config.get('minimumInstructionCoverage')  // Optional
+    def minimumLineCoverage = config.get('minimumLineCoverage')  // Optional
+    def minimumMethodCoverage = config.get('minimumMethodCoverage')  // Optional
 
     // Maximum Coverage thresholds (optional, no default values)
-    def maximumBranchCoverage = config.get('maximumBranchCoverage')  // Optional, Integer or String
-    def maximumClassCoverage = config.get('maximumClassCoverage')  // Optional, Integer or String
-    def maximumComplexityCoverage = config.get('maximumComplexityCoverage')  // Optional, Integer or String
-    def maximumInstructionCoverage = config.get('maximumInstructionCoverage')  // Optional, Integer or String
-    def maximumLineCoverage = config.get('maximumLineCoverage')  // Optional, Integer or String
-    def maximumMethodCoverage = config.get('maximumMethodCoverage')  // Optional, Integer or String
-
-    // Ensure coverage thresholds and delta values are passed as strings if necessary (if they're integers)
-    def convertToString = { value ->
-        value != null ? value.toString() : null
-    }
-
-    minimumBranchCoverage = convertToString(minimumBranchCoverage)
-    minimumClassCoverage = convertToString(minimumClassCoverage)
-    minimumComplexityCoverage = convertToString(minimumComplexityCoverage)
-    minimumInstructionCoverage = convertToString(minimumInstructionCoverage)
-    minimumLineCoverage = convertToString(minimumLineCoverage)
-    minimumMethodCoverage = convertToString(minimumMethodCoverage)
-
-    maximumBranchCoverage = convertToString(maximumBranchCoverage)
-    maximumClassCoverage = convertToString(maximumClassCoverage)
-    maximumComplexityCoverage = convertToString(maximumComplexityCoverage)
-    maximumInstructionCoverage = convertToString(maximumInstructionCoverage)
-    maximumLineCoverage = convertToString(maximumLineCoverage)
-    maximumMethodCoverage = convertToString(maximumMethodCoverage)
-
-    deltaBranchCoverage = convertToString(deltaBranchCoverage)
-    deltaClassCoverage = convertToString(deltaClassCoverage)
-    deltaComplexityCoverage = convertToString(deltaComplexityCoverage)
-    deltaInstructionCoverage = convertToString(deltaInstructionCoverage)
-    deltaLineCoverage = convertToString(deltaLineCoverage)
-    deltaMethodCoverage = convertToString(deltaMethodCoverage)
+    def maximumBranchCoverage = config.get('maximumBranchCoverage')  // Optional
+    def maximumClassCoverage = config.get('maximumClassCoverage')  // Optional
+    def maximumComplexityCoverage = config.get('maximumComplexityCoverage')  // Optional
+    def maximumInstructionCoverage = config.get('maximumInstructionCoverage')  // Optional
+    def maximumLineCoverage = config.get('maximumLineCoverage')  // Optional
+    def maximumMethodCoverage = config.get('maximumMethodCoverage')  // Optional
 
     // Build Jacoco step with all parameters
-    jacocoReport(
+    jacoco(
         execPattern: execPattern,
         classPattern: classPattern,
         sourcePattern: sourcePattern,
