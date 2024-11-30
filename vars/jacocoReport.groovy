82ce3af72810a5fb9935a024bccda30ecf4de371
naresh240 <naresh240.qis@gmail.com>
2024-11-30 22:05:14 +0530

    added jacoco report groovy

--- vars/jacocoReport.groovy
+++ vars/jacocoReport.groovy
@@ -1,41 +1,60 @@
-// vars/jacocoReport.groovy
 def call(Map config = [:]) {
     // Default configurations
-    def execPattern = config.get('execPattern', '**/jacoco.exec')
-    def classPattern = config.get('classPattern', '**/classes')
-    def sourcePattern = config.get('sourcePattern', '**/src/main/java')
+    def execPattern = config.get('execPattern', '**/jacoco.exec') // String
+    def classPattern = config.get('classPattern', '**/classes') // String
+    def sourcePattern = config.get('sourcePattern', '**/src/main/java') // String
 
-    def exclusionPattern = config.get('exclusionPattern')  // Optional
-    def inclusionPattern = config.get('inclusionPattern')  // Optional
-    def runAlways = config.get('runAlways')  // Optional
-    def skipCopyOfSrcFiles = config.get('skipCopyOfSrcFiles')  // Optional
-    def sourceExclusionPattern = config.get('sourceExclusionPattern')  // Optional
-    def buildOverBuild = config.get('buildOverBuild')  // Optional
-    def changeBuildStatus = config.get('changeBuildStatus')  // Optional
+    def exclusionPattern = config.get('exclusionPattern')  // Optional, String
+    def inclusionPattern = config.get('inclusionPattern')  // Optional, String
+    def runAlways = config.get('runAlways')  // Optional, Boolean
+    def skipCopyOfSrcFiles = config.get('skipCopyOfSrcFiles')  // Optional, Boolean
+    def sourceExclusionPattern = config.get('sourceExclusionPattern')  // Optional, String
+    def buildOverBuild = config.get('buildOverBuild')  // Optional, Boolean
+    def changeBuildStatus = config.get('changeBuildStatus')  // Optional, Boolean
 
     // Delta Thresholds (optional, no default values)
-    def deltaBranchCoverage = config.get('deltaBranchCoverage')  // Optional
-    def deltaClassCoverage = config.get('deltaClassCoverage')  // Optional
-    def deltaComplexityCoverage = config.get('deltaComplexityCoverage')  // Optional
-    def deltaInstructionCoverage = config.get('deltaInstructionCoverage')  // Optional
-    def deltaLineCoverage = config.get('deltaLineCoverage')  // Optional
-    def deltaMethodCoverage = config.get('deltaMethodCoverage')  // Optional
+    def deltaBranchCoverage = config.get('deltaBranchCoverage')  // Optional, Integer or String
+    def deltaClassCoverage = config.get('deltaClassCoverage')  // Optional, Integer or String
+    def deltaComplexityCoverage = config.get('deltaComplexityCoverage')  // Optional, Integer or String
+    def deltaInstructionCoverage = config.get('deltaInstructionCoverage')  // Optional, Integer or String
+    def deltaLineCoverage = config.get('deltaLineCoverage')  // Optional, Integer or String
+    def deltaMethodCoverage = config.get('deltaMethodCoverage')  // Optional, Integer or String
 
     // Minimum Coverage thresholds (optional, no default values)
-    def minimumBranchCoverage = config.get('minimumBranchCoverage')  // Optional
-    def minimumClassCoverage = config.get('minimumClassCoverage')  // Optional
-    def minimumComplexityCoverage = config.get('minimumComplexityCoverage')  // Optional
-    def minimumInstructionCoverage = config.get('minimumInstructionCoverage')  // Optional
-    def minimumLineCoverage = config.get('minimumLineCoverage')  // Optional
-    def minimumMethodCoverage = config.get('minimumMethodCoverage')  // Optional
+    def minimumBranchCoverage = config.get('minimumBranchCoverage')  // Optional, Integer or String
+    def minimumClassCoverage = config.get('minimumClassCoverage')  // Optional, Integer or String
+    def minimumComplexityCoverage = config.get('minimumComplexityCoverage')  // Optional, Integer or String
+    def minimumInstructionCoverage = config.get('minimumInstructionCoverage')  // Optional, Integer or String
+    def minimumLineCoverage = config.get('minimumLineCoverage')  // Optional, Integer or String
+    def minimumMethodCoverage = config.get('minimumMethodCoverage')  // Optional, Integer or String
 
     // Maximum Coverage thresholds (optional, no default values)
-    def maximumBranchCoverage = config.get('maximumBranchCoverage')  // Optional
-    def maximumClassCoverage = config.get('maximumClassCoverage')  // Optional
-    def maximumComplexityCoverage = config.get('maximumComplexityCoverage')  // Optional
-    def maximumInstructionCoverage = config.get('maximumInstructionCoverage')  // Optional
-    def maximumLineCoverage = config.get('maximumLineCoverage')  // Optional
-    def maximumMethodCoverage = config.get('maximumMethodCoverage')  // Optional
+    def maximumBranchCoverage = config.get('maximumBranchCoverage')  // Optional, Integer or String
+    def maximumClassCoverage = config.get('maximumClassCoverage')  // Optional, Integer or String
+    def maximumComplexityCoverage = config.get('maximumComplexityCoverage')  // Optional, Integer or String
+    def maximumInstructionCoverage = config.get('maximumInstructionCoverage')  // Optional, Integer or String
+    def maximumLineCoverage = config.get('maximumLineCoverage')  // Optional, Integer or String
+    def maximumMethodCoverage = config.get('maximumMethodCoverage')  // Optional, Integer or String
+
+    // Ensure coverage thresholds are passed as strings if necessary (if they're integers)
+    if (minimumBranchCoverage != null) {
+        minimumBranchCoverage = minimumBranchCoverage.toString()
+    }
+    if (minimumClassCoverage != null) {
+        minimumClassCoverage = minimumClassCoverage.toString()
+    }
+    if (minimumComplexityCoverage != null) {
+        minimumComplexityCoverage = minimumComplexityCoverage.toString()
+    }
+    if (minimumInstructionCoverage != null) {
+        minimumInstructionCoverage = minimumInstructionCoverage.toString()
+    }
+    if (minimumLineCoverage != null) {
+        minimumLineCoverage = minimumLineCoverage.toString()
+    }
+    if (minimumMethodCoverage != null) {
+        minimumMethodCoverage = minimumMethodCoverage.toString()
+    }
 
     // Build Jacoco step with all parameters
     jacoco(
