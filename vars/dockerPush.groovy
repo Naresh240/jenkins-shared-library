978428e2d7c5fb4b5ecd983b97ae005b900dacbf
naresh240 <naresh240.qis@gmail.com>
2024-12-02 14:58:20 +0530

    updated trivy

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,7 +1,5 @@
 def push(Map stageParams) {
-    sh """
-        docker login -u '$username' -p '$password'
-        docker image push $stageParams.dockerUser/$stageParams.imageName:$stageParams.imageTag
-        docker image push $stageParams.dockerUser/$stageParams.imageName:latest
-    """
+    sh "docker login -u '$username' -p '$password'"
+    sh "docker image push $stageParams.dockerUser/$stageParams.imageName:$stageParams.imageTag"
+    sh "docker image push $stageParams.dockerUser/$stageParams.imageName:latest"
 }
