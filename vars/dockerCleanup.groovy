3017ecf9c7192cb12f2deb313f763d5094837759
naresh240 <naresh240.qis@gmail.com>
2024-12-02 13:33:20 +0530

    updated groovy script

--- vars/dockerCleanup.groovy
+++ vars/dockerCleanup.groovy
@@ -1,4 +1,4 @@
 def call(Map stageParams) {
-    sh "docker rmi ${stageParams.dockerUser}/${stageParams.imageName}:${stageParams.imageTag}"
-    sh "docker rmi ${stageParams.dockerUser}/${stageParams.imageName}:latest"
+    sh "docker rmi $stageParams.dockerUser/$stageParams.imageName:$stageParams.imageTag"
+    sh "docker rmi $stageParams.dockerUser/$stageParams.imageName:latest"
 }
